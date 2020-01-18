5aa6e3e0f32672a462bc2ffffa6f5b448bb8a80f
gasparpd <gasparzurea1604@gmail.com>
2020-01-18 22:37:40 +0100

    Terminado ejercicio 1 con la implementación del script de borrado de la base de datos

--- PracticaEV2/src/Principal.java
+++ PracticaEV2/src/Principal.java
@@ -8,15 +8,31 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[]args){
         Scanner teclado = new Scanner(System.in);
-        System.out.println("--------------------MENU-------------------\n" +
-                "1 - Crea tablas\n" +
-                "2 - Insertar datos de prueba\n" +
-                "-------------------------------------------");
-        int res = teclado.nextInt();
-        if(res == 1){
-            crearTablas();
-        } else if (res == 2){
-            datosPrueba();
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("--------------------MENU-------------------\n" +
+                    "1 - Crea tablas.\n" +
+                    "2 - Insertar datos de prueba.\n" +
+                    "3 - Eliminar base de datos.\n" +
+                    "0 - Salir.\n" +
+                    "-------------------------------------------");
+            int res = teclado.nextInt();
+            switch (res){
+                case 1:
+                    crearTablas();
+                    break;
+                case 2:
+                    datosPrueba();
+                    break;
+                case 3:
+                    borrarDB();
+                    break;
+                case 0:
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Operación no válida.");
+            }
         }
     }
 
@@ -70,6 +86,54 @@ public class Principal {
     }
     public static void datosPrueba() {
         File script = new File("./script/smartphones(datos-prueba).sql");
+        System.out.println("--------------------------------------------");
+        System.out.println("\n\nFichero de consulta : " + script.getName());
+        System.out.println("Convirtiendo el fichero a cadena...");
+
+        BufferedReader entrada = null;
+        try {
+            entrada = new BufferedReader(new FileReader(script));
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR NO ENCUENTRA el fichero: " + e.getMessage());
+            e.printStackTrace();
+        }
+        String linea = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        String salto = System.getProperty("line.separator");
+        try {
+            while ((linea = entrada.readLine()) != null) {
+                stringBuilder.append(linea);
+                stringBuilder.append(salto);
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR de E/S, al operar con el fichero: " + e.getMessage());
+            e.printStackTrace();
+        }
+        String consulta = stringBuilder.toString();
+
+        System.out.println(consulta);
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ERROR En el Driver: " + e.getMessage());
+            e.printStackTrace();
+        }
+        try {
+            Connection connmysql = DriverManager
+                    .getConnection("jdbc:mysql://localhost/smartphones?allowMultiQueries=true", "ejemplo", "ejemplo");
+            Statement sents = connmysql.createStatement();
+            int res = sents.executeUpdate(consulta);
+            System.out.println("Script creado con éxito, res = " + res);
+            connmysql.close();
+            sents.close();
+        } catch (SQLException e) {
+            System.out.println("ERROR AL EJECUTAR EL SCRIPT: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    public static void borrarDB() {
+        File script = new File("./script/borrarbasedatos.sql");
         System.out.println("--------------------------------------------");
         System.out.println("\n\nFichero de consulta : " + script.getName());
         System.out.println("Convirtiendo el fichero a cadena...");
