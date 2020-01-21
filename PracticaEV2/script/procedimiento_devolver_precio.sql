4007e20ca80fc6de4c6802f2e19f1511bf7d4ba8
gasparpd <gasparzurea1604@gmail.com>
2020-01-21 10:28:03 +0100

    Eliminado un script de procedimiento almacenado

--- PracticaEV2/script/procedimiento_devolver_precio.sql
+++
@@ -1,8 +0,0 @@
-delimiter //
-CREATE PROCEDURE devolver_precio_mayor(p int, OUT model VARCHAR(20), OUT price int(5))
-BEGIN
-    SET model = 'INEXISTENTE';
-    SET prize = 0;
-    SELECT MODELO, PRECIO INTO model, price FROM smartphone WHERE PRECIO >= p
-END //
-delimiter ;
