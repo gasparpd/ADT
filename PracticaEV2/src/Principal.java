522f5b504e1a344a5880530b0e23d386a8ea95d9
gasparpd <you@example.com>
2020-01-22 20:24:23 +0100

    Cambio de la variable teclado a private. Lista para entregar la primera parte.

--- PracticaEV2/src/Principal.java
+++ PracticaEV2/src/Principal.java
@@ -3,7 +3,7 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Principal {
-    static Scanner teclado;
+    private static Scanner teclado;
 
     public static void main(String[]args){
         teclado = new Scanner(System.in);
