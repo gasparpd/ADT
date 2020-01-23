7cc307ca0045f0a85dfb6f8012ecbff8fb84b9cc
gasparpd <you@example.com>
2020-01-23 20:45:36 +0100

    Segunda parte ejercicio 14

--- EjerciciosEV2/src/ejercicio14/Main.java
+++ EjerciciosEV2/src/ejercicio14/Main.java
@@ -1,6 +1,7 @@
 package ejercicio14;
 
 import java.sql.*;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String [] args){
@@ -20,9 +21,15 @@ public class Main {
                     " ON empleados.dept_no = departamentos.dept_no\n" +
                     " WHERE empleados.dept_no = ?\n";
 
+            String sql_sal_medio = "SELECT COUNT(apellido), AVG(salario)\n" +
+                    "FROM empleados\n" +
+                    "WHERE dept_no = ?\n";
+
             System.out.println(sql);
             PreparedStatement sentencia = conexion.prepareStatement(sql);
+            PreparedStatement sent_sal = conexion.prepareStatement(sql_sal_medio);
             sentencia.setInt(1, Integer.parseInt(dep_no));
+            sent_sal.setInt(1, Integer.parseInt(dep_no));
 
             try {
                 ResultSet rs = sentencia.executeQuery();
@@ -33,7 +40,15 @@ public class Main {
                             rs.getString(3),
                             rs.getString(4));
                 }
-                System.out.println("---------FIN---------");
+                System.out.println("---------FIN EMPLEADOS---------");
+                //Consulta de salario medio
+                rs = sent_sal.executeQuery();
+                while (rs.next()){
+                    DecimalFormat formato = new DecimalFormat("##,##0.00");
+                    String valorFormateado = formato.format(rs.getFloat(1));
+                    System.out.printf("Número de empleados: %d, Salario medio: %s\n", rs.getInt(1), formato.format(rs.getFloat(2));
+                }
+                System.out.println("---------FIN----------");
             }catch (SQLException e) {
                 System.out.println("HA OCURRIDO UNA EXCEPCIÓN:");
                 System.out.println("Mensaje:    "+ e.getMessage());
