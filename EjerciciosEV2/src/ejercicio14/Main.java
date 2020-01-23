6814844f871c0a145247ccd91b856b83153312ab
gasparpd <you@example.com>
2020-01-23 20:22:45 +0100

    Terminada la primera parte del ejercicio 14.

--- EjerciciosEV2/src/ejercicio14/Main.java
+++ EjerciciosEV2/src/ejercicio14/Main.java
@@ -1,9 +1,6 @@
 package ejercicio14;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String [] args){
@@ -17,11 +14,32 @@ public class Main {
             String dep_no = args[0]; // departamento
 
             // construir orden INSERT
-            String sql = " SELECT apellido, salario, oficio FROM `empleados` WHERE dept_no = ?";
+            String sql = " SELECT empleados.apellido, empleados.salario, empleados.oficio, departamentos.dnombre\n" +
+                    " FROM empleados\n" +
+                    " INNER JOIN departamentos\n" +
+                    " ON empleados.dept_no = departamentos.dept_no\n" +
+                    " WHERE empleados.dept_no = ?\n";
 
             System.out.println(sql);
             PreparedStatement sentencia = conexion.prepareStatement(sql);
             sentencia.setInt(1, Integer.parseInt(dep_no));
+
+            try {
+                ResultSet rs = sentencia.executeQuery();
+                while (rs.next()) {
+                    System.out.printf("Apellido: %s, Salario: %f€, Oficio: %s, Nombre departamento: %s\n",
+                            rs.getString(1),
+                            rs.getFloat(2),
+                            rs.getString(3),
+                            rs.getString(4));
+                }
+                System.out.println("---------FIN---------");
+            }catch (SQLException e) {
+                System.out.println("HA OCURRIDO UNA EXCEPCIÓN:");
+                System.out.println("Mensaje:    "+ e.getMessage());
+                System.out.println("SQL estado: "+ e.getSQLState());
+                System.out.println("Cód error:  "+ e.getErrorCode());
+            }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
