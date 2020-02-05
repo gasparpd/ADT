5977d0ff8d8ec58da9ff7e7549d257a4071d5fe6
gasparpd <gasparzurea1604@gmail.com>
2020-02-05 14:30:37 +0100

    Mejorado el menú

--- ProyectoFicheros/src/ficherosPropios/App.java
+++ ProyectoFicheros/src/ficherosPropios/App.java
@@ -1,13 +1,50 @@
 package ficherosPropios;
 
+import java.util.Scanner;
+
 public class App {
     public static void Main (String [] args) {
-        System.out.println("----------------MENU----------------" +
-                "1. Leer registro sin parámetro." +
-                "2. Leer registro son parámetro." +
-                "3. Escribir registro sobreescribiendo." +
-                "4. Escribir registro a continuación." +
-                "5. Mostrar número de registros." +
-                "6. Imprimir los registros.");
+        Scanner input = new Scanner(System.in);
+        boolean salir = false;
+        int accion;
+        do {
+            System.out.println("----------------MENU----------------" +
+                    "1. Establecer fichero." +
+                    "2. Mostrar número de registros." +
+                    "3. Imprimir los registros." +
+                    "4. Escribir registro a continuación." +
+                    "5. Escribir registro sobreescribiendo." +
+                    "6. Borrar registro." +
+                    "7. Borrar fichero de datos." +
+                    "8. Buscar registro." +
+                    "0. Salir." +
+                    "------------------------------------");
+            accion = input.nextInt();
+            switch (accion){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 0:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("El número no corresponde con ninguna acción.");
+                    break;
+
+            }
+        }while(!salir);
     }
 }
