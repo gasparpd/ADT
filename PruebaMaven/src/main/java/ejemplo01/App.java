7f230768c74b269f332de02c20a2ce0c1be6907c
gasparpd <you@example.com>
2020-02-05 20:53:07 +0100

    Terminado ejercicio 8

--- PruebaMaven/src/main/java/ejemplo01/App.java
+++ PruebaMaven/src/main/java/ejemplo01/App.java
@@ -1,5 +1,6 @@
 package ejemplo01;
 
+import org.hibernate.ObjectNotFoundException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -7,6 +8,12 @@ import org.hibernate.Transaction;
 import javax.persistence.PersistenceException;
 
 public class App {
+    public static void main(String [] args) {
+        //save();
+        //leerProfesor();
+        leerProfesorLoad();
+    }
+
     private static void save() {
         try {
             //Obtenemos el SessionFactory
@@ -30,7 +37,44 @@ public class App {
         }
     }
 
-    public static void main(String [] args) {
-        save();
+    public static void leerProfesor() {
+        //Obtenemos el SessionFactory
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        //Abrimos la sesión mediante el SessionFactory
+
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            Profesor profesor = (Profesor) session.get(Profesor.class, 102);
+            System.out.println("Profesor: " + profesor.getNombre());
+        }catch (NullPointerException e){
+            System.out.println("El identificador no ha sido encontrado en la base de datos.");
+        }
+
+        tx.commit();
+        session.close();
+        sessionFactory.close();
+    }
+
+    public static void leerProfesorLoad() {
+        //Obtenemos el SessionFactory
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        //Abrimos la sesión mediante el SessionFactory
+        Session session = sessionFactory.openSession();
+
+        Transaction tx = session.beginTransaction();
+        Profesor profesor = new Profesor();
+        try {
+            profesor = (Profesor) session.load(Profesor.class, 102);
+            System.out.println("Profesor: " + profesor.getNombre());
+        } catch (org.hibernate.ObjectNotFoundException e) {
+            System.out.println("El valor clave no se ha encontrado en la base de datos.");
+        }
+
+        tx.commit();
+        session.close();
+        sessionFactory.close();
     }
 }
