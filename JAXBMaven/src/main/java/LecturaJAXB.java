8e229b9f555ad09c2331b4cd2eb31cb978fa5228
gasparpd <gasparzurea1604@gmail.com>
2020-02-06 13:33:18 +0100

    Ejemplo lectura XML librerías con JAXB (Proyecto Maven)

---
+++ JAXBMaven/src/main/java/LecturaJAXB.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+public class LecturaJAXB {
+
+	public static void main(String[] args) throws JAXBException {
+		JAXBContext context = JAXBContext.newInstance(Libreria.class);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		Libreria libreria = (Libreria) unmarshaller.unmarshal(new File("./src/main/resources/Ficheros/libreria.xml"));
+
+		System.out.println("Nombre: "+libreria.getNombre());
+
+		ArrayList<Libro> libros = libreria.getLibros();
+
+		for(Libro l: libros){
+			System.out.println(l.getIsbn()+" "+l.getTitulo()+" "+l.getAutor());
+		}
+	}
+}
