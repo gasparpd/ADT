19358bc58e705c4060a2a4278dbcf0f9c5bb2ffc
gasparpd <you@example.com>
2020-02-13 19:29:52 +0100

    Preparado para ejecutar desde la app.

--- PracticaEV2/src/EscrituraDOM.java
+++ PracticaEV2/src/EscrituraDOM.java
@@ -3,11 +3,13 @@ import javax.xml.transform.TransformerException;
 import java.io.IOException;
 
 public class EscrituraDOM {
-    public static void main(String [] args) throws ParserConfigurationException, TransformerException, IOException {
-        String claseFab = "fabricantes";
-        String claseSmart = "smartphones";
-        GeneradorDOM generadorDOM = new GeneradorDOM();
-        generadorDOM.generarDocument(claseSmart);
-        generadorDOM.generarXML(claseSmart);
+    public static void main(String tabla) {
+        try {
+            GeneradorDOM generadorDOM = new GeneradorDOM();
+            generadorDOM.generarDocument(tabla);
+            generadorDOM.generarXML(tabla);
+        } catch (TransformerException | IOException | ParserConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 }
