7c441b662a7c0c718368daa7d923e946bdcea95f
gasparpd <gasparzurea1604@gmail.com>
2020-02-17 15:30:31 +0100

    Hecha la lectura de fabricantes.xml y cargado el ArrayList.

--- PracticaEV2/src/LecturaDOM.java
+++ PracticaEV2/src/LecturaDOM.java
@@ -6,51 +6,54 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.ArrayList;
 
 public class LecturaDOM {
+    private static ArrayList<Smartphone> smartphones;
+    private static ArrayList<Fabricante> fabricantes;
 
     public static void main(String tabla) {
-
 
         DocumentBuilderFactory factory =
                 DocumentBuilderFactory.newInstance();
 
         try {
             DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse(new File("Empleados.xml"));
+            Document document = builder.parse(new File("./Ficheros/" + tabla + ".xml"));
             document.getDocumentElement().normalize();
 
-            System.out.printf("Elemento raiz: %s %n",
-                    document.getDocumentElement().getNodeName());
-            //crea una lista con todos los nodos empleado
-            NodeList empleados = document.getElementsByTagName("empleado");
-            System.out.printf("Nodos empleado a recorrer: %d %n",
-                    empleados.getLength());
+            /*System.out.printf("Elemento raiz: %s %n",
+                    document.getDocumentElement().getNodeName());*/
 
-            //recorrer la lista
-            for (int i = 0; i < empleados.getLength(); i++) {
-                Node emple = empleados.item(i); //obtener un nodo empleado
-                if (emple.getNodeType() == Node.ELEMENT_NODE) {//tipo de nodo
-                    //obtener los elementos del nodo
-                    Element elemento = (Element) emple;
-                    System.out.printf("ID = %s %n",
-                            elemento.getElementsByTagName("id").
-                                    item(0).getTextContent());
-                    System.out.printf(" * Apellido = %s %n",
-                            elemento.getElementsByTagName("apellido").
-                                    item(0).getTextContent());
-                    System.out.printf(" * Departamento = %s %n",
-                            elemento.getElementsByTagName("dep").
-                                    item(0).getTextContent());
-                    System.out.printf(" * Salario = %s %n",
-                            elemento.getElementsByTagName("salario").
-                                    item(0).getTextContent());
+            //crea una lista con todos los nodos FABRICANTE o SMARTPHONE
+            NodeList fabsNodeList = null, smartNodeList = null;
+            if (tabla.equalsIgnoreCase("fabricantes")) {
+                fabsNodeList = document.getElementsByTagName("fabricante");
+            } else {
+                smartNodeList = document.getElementsByTagName("smartphone");
+            }
+
+            /*Recorremos los NodeList obtenidos y cargamos los arrays*/
+            if (tabla.equalsIgnoreCase("fabricantes")) {
+                for (int c = 0; c < fabsNodeList.getLength(); c++) {
+                    Node fab = fabsNodeList.item(c);
+
+                    if (fab.getNodeType() == Node.ELEMENT_NODE) {
+                        Element element = (Element) fab;
+                        int id = Integer.parseInt(element.getAttribute("id"));
+                        String nombre = element.getElementsByTagName("nombre").
+                                item(0).getTextContent();
+                        String f_year = element.getElementsByTagName("foundation_year").
+                                item(0).getTextContent();
+                        int matriz = Integer.parseInt(element.getElementsByTagName("matriz").
+                                item(0).getTextContent());
+                        Fabricante fabricante = new Fabricante(id, nombre, f_year, matriz);
+                        fabricantes.add(fabricante);
+                    }
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 }
