b3d3a7bf74298f7e35b3014c64a0e5385215f2dc
gasparpd <you@example.com>
2020-03-05 20:20:53 +0100

    Ejercicio 17 terminado

---
+++ EjerciciosHibernate/src/main/java/ejemplo01/App5.java
@@ -0,0 +1,170 @@
+package ejemplo01;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import javax.persistence.PersistenceException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class App5 {
+    public static void main(String[] args) {
+        //guardarProfesor();
+        //leerProfesor();
+        //leerProfesorLoad();
+        //modificarProfesor();
+        //borrarProfesor();
+        guardarOActualizarProfesor();
+        //queryClass();
+    }
+
+    private static void guardarProfesor() {
+        try {
+            //Obtenemos el SessionFactory
+
+            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+            //Abrimos la sesión mediante el SessionFactory
+            Session session = sessionFactory.openSession();
+
+            //Creamos el objeto
+            Profesor4 profesor = new Profesor4(101, "Alberto", "Perez", "García");
+            Transaction tx = session.beginTransaction();
+
+            session.save(profesor);//Aquí guardamos el objeto en la base de datos.
+
+            tx.commit();
+            session.close();
+            sessionFactory.close();
+        } catch (PersistenceException e) {
+            System.out.println("Clave primaria duplicada.");
+        }
+    }
+
+    public static void leerProfesor() {
+        //Obtenemos el SessionFactory
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        //Abrimos la sesión mediante el SessionFactory
+
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            Profesor4 profesor = session.get(Profesor4.class, 102);
+            System.out.println("Profesor: " + profesor.getNombre());
+        } catch (NullPointerException e) {
+            System.out.println("El identificador no ha sido encontrado en la base de datos.");
+        }
+
+        tx.commit();
+        session.close();
+        sessionFactory.close();
+    }
+
+    public static void leerProfesorLoad() {
+        //Obtenemos el SessionFactory
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        //Abrimos la sesión mediante el SessionFactory
+        Session session = sessionFactory.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+        try {
+            Profesor4 profesor = session.load(Profesor4.class, 102);
+            System.out.println("Profesor: " + profesor.getNombre());
+        } catch (org.hibernate.ObjectNotFoundException e) {
+            System.out.println("El valor clave no se ha encontrado en la base de datos.");
+        }
+
+        tx.commit();
+        session.close();
+        sessionFactory.close();
+    }
+
+    public static void modificarProfesor() {
+        //Obtenemos el SessionFactory
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        //Abrimos la sesión mediante el SessionFactory
+        Session session = sessionFactory.openSession();
+
+        Transaction tx = session.beginTransaction();
+        try {
+            Profesor4 profesor = session.get(Profesor4.class, 101);
+            System.out.println("Profesor: " + profesor.getNombre());
+            System.out.println("Profesor: " + profesor.getNombre());
+            profesor.setNombre("Pedro");
+            session.update(profesor);
+        } catch (NullPointerException e) {
+            System.out.println("El identificador no ha sido encontrado en la base de datos.");
+        }
+
+        tx.commit();
+        session.close();
+        sessionFactory.close();
+    }
+
+    public static void borrarProfesor() {
+        //Obtenemos el SessionFactory
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        //Abrimos la sesión mediante el SessionFactory
+        Session session = sessionFactory.openSession();
+
+        Transaction tx = session.beginTransaction();
+        try {
+            Profesor4 profesor = session.get(Profesor4.class, 101);
+            System.out.println("Profesor:" + profesor.getNombre());
+            session.delete(profesor);
+        } catch (NullPointerException e) {
+            System.out.println("ID no encontrado en la base de datos.");
+        }
+        tx.commit();
+        session.close();
+        sessionFactory.close();
+    }
+
+    public static void guardarOActualizarProfesor() {
+
+        Profesor5 profesor = new Profesor5(7, "Sara", "Barrrera", "Salas");
+        Set <CorreoElectronico5> correosElectronicos = new HashSet<CorreoElectronico5>();
+        correosElectronicos.add(new CorreoElectronico5(3, "sara@yahoo.com", profesor));
+        correosElectronicos.add(new CorreoElectronico5(2, "sara@hotmail.com", profesor));
+        correosElectronicos.add(new CorreoElectronico5(1, "sara@gmail.com", profesor));
+
+        profesor.setCorreosElectronicos(correosElectronicos);
+
+        //Obtenemos el SessionFactory
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        //Abrimos la sesión mediante el SessionFactory
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.save(profesor);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public static void queryClass() {
+        //Obtenemos el SessionFactory
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        //Abrimos la sesión mediante el SessionFactory
+        Session session = sessionFactory.openSession();
+
+        Query query = session.createQuery("SELECT p FROM Profesor p");
+        List<Profesor> profesores = query.list();
+        for (Profesor profesor : profesores) {
+            System.out.println(profesor.getNombre());
+        }
+
+        session.close();
+        sessionFactory.close();
+    }
+}
--- EjerciciosHibernate/src/main/java/ejemplo01/CorreoElectronico5.java
+++ EjerciciosHibernate/src/main/java/ejemplo01/CorreoElectronico5.java
@@ -5,14 +5,38 @@ import java.io.Serializable;
 public class CorreoElectronico5 implements Serializable {
     private int idCorreo;
     private String direccionCorreo;
-    private Profesor profesor;
+    private Profesor5 profesor;
 
     public CorreoElectronico5(){
     }
 
-    public CorreoElectronico5(int idCorreo, String direccionCorreo, Profesor profesor){
+    public CorreoElectronico5(int idCorreo, String direccionCorreo, Profesor5 profesor){
         this.idCorreo=idCorreo;
         this.direccionCorreo=direccionCorreo;
         this.profesor=profesor;
+    }
+
+    public int getIdCorreo() {
+        return idCorreo;
+    }
+
+    public void setIdCorreo(int idCorreo) {
+        this.idCorreo = idCorreo;
+    }
+
+    public String getDireccionCorreo() {
+        return direccionCorreo;
+    }
+
+    public void setDireccionCorreo(String direccionCorreo) {
+        this.direccionCorreo = direccionCorreo;
+    }
+
+    public Profesor5 getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Profesor5 profesor) {
+        this.profesor = profesor;
     }
 }
