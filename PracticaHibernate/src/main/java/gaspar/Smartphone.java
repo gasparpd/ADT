b2e966dae7ef527f411316eec8c37953c3e2bdc6
gasparpd <you@example.com>
2020-03-09 20:23:51 +0100

    Implementando Serializable

--- PracticaHibernate/src/main/java/gaspar/Smartphone.java
+++ PracticaHibernate/src/main/java/gaspar/Smartphone.java
@@ -4,10 +4,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "smartphone")
-public class Smartphone {
+public class Smartphone implements Serializable {
 
     @Id
     @Column(name = "ID_SMARTPHONE")
