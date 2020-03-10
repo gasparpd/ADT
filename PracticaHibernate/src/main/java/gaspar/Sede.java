fdb08ece1a3822538cf90f886b52e727e4aee2b2
gasparpd <gasparzurea1604@gmail.com>
2020-03-10 12:21:46 +0100

    Creada clase sede

---
+++ PracticaHibernate/src/main/java/gaspar/Sede.java
@@ -0,0 +1,52 @@
+package gaspar;
+
+import java.io.Serializable;
+
+public class Sede implements Serializable {
+    private int id;
+    private String pais;
+    private String localidad;
+    private String cpostal;
+
+    public Sede(int id, String pais, String localidad, String cpostal) {
+        this.id = id;
+        this.pais = pais;
+        this.localidad = localidad;
+        this.cpostal = cpostal;
+    }
+
+    public Sede() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public String getCpostal() {
+        return cpostal;
+    }
+
+    public void setCpostal(String cpostal) {
+        this.cpostal = cpostal;
+    }
+}
