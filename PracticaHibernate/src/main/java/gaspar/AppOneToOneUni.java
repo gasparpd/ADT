7f05677efbeb6f1060e111ca728cf92432b94717
gasparpd <gasparzurea1604@gmail.com>
2020-03-11 22:26:08 +0100

    Implementado el método de 'saveOrUpdate' para guardar o actualizar un fabricante

--- PracticaHibernate/src/main/java/gaspar/AppOneToOneUni.java
+++ PracticaHibernate/src/main/java/gaspar/AppOneToOneUni.java
@@ -14,7 +14,7 @@ public class AppOneToOneUni {
         //loadFabricante();     //Hecho
         //updateFabricante();   //Hecho
         //deleteFabricante();   //Hecho
-        //guardarOActualizarFabricante();
+        //saveOrUpdateFabricante(); //Hecho
         //queryClass();
     }
 
@@ -30,7 +30,7 @@ public class AppOneToOneUni {
 
             //Creamos el objeto FabricanteSede y le pasamos su sede
             FabricanteSede fab = new FabricanteSede(1, "Apple", "1997", null);
-            Sede sede = new Sede( 1, "España", "Madrid", "21012");
+            Sede sede = new Sede(1, "España", "Madrid", "21012");
             fab.setSede(sede);
 
             session.save(fab);  //Guardamos el objeto en la base de datos.
@@ -56,7 +56,7 @@ public class AppOneToOneUni {
         try {
             FabricanteSede fab = session.get(FabricanteSede.class, 1);
             System.out.println("Fabricante: " + fab.getNombre());
-            System.out.println("Sede: " +fab.getSede().toString());
+            System.out.println("Sede: " + fab.getSede().toString());
         } catch (NullPointerException e) {
             System.out.println("El identificador no ha sido encontrado en la base de datos.");
         }
@@ -79,7 +79,7 @@ public class AppOneToOneUni {
         try {
             FabricanteSede fab = session.load(FabricanteSede.class, 1);
             System.out.println("Fabricante: " + fab.getNombre());
-            System.out.println("Sede: " +fab.getSede().toString());
+            System.out.println("Sede: " + fab.getSede().toString());
         } catch (org.hibernate.ObjectNotFoundException e) {
             System.out.println("El valor clave no se ha encontrado en la base de datos.");
         }
@@ -139,33 +139,32 @@ public class AppOneToOneUni {
         } catch (NullPointerException e) {
             System.out.println("ID no encontrado en la base de datos.");
         }
+
         tx.commit();
         session.close();
         sessionFactory.close();
     }
 
-    /*public static void guardarOActualizarFabricante() {
-
-        FabricanteSede fab = new FabricanteSede(7, "Sara", "Barrrera", "Salas");
-        List<CorreoElectronico6> correosElectronicos = new ArrayList<CorreoElectronico6>();
-        correosElectronicos.add(new CorreoElectronico6(3, "sara@yahoo.com", fab));
-        correosElectronicos.add(new CorreoElectronico6(2, "sara@hotmail.com", fab));
-        correosElectronicos.add(new CorreoElectronico6(1, "sara@gmail.com", fab));
-
-        fab.setCorreosElectronicos(correosElectronicos);
-
+    public static void saveOrUpdateFabricante() {
         //Obtenemos el SessionFactory
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
         //Abrimos la sesión mediante el SessionFactory
         Session session = sessionFactory.openSession();
+
         session.beginTransaction();
 
-        session.save(fab);
+        FabricanteSede fab = new FabricanteSede(2, "BQ", "2002", null);
+
+        // Creamos y añadimos la sede al fabricante
+        Sede sede = new Sede(fab.getId(), "SPAIN", "MADRID", "400392");
+        fab.setSede(sede);//Le añadimos al fabricante la nueva sede creada
+
+        session.saveOrUpdate(fab);
 
         session.getTransaction().commit();
         session.close();
-    }*/
+    }
 
     /*public static void queryClass() {
         //Obtenemos el SessionFactory
